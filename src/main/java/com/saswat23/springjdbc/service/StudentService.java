ad0e27889ac09a448cdaf5e4ab258c8ccf42a840
saswat-23 <saswat23@gmail.com>
2024-05-20 01:52:30 +0530

    [ERROR] No Bean for StudentRepo

--- src/main/java/com/saswat23/springjdbc/service/StudentService.java
+++ src/main/java/com/saswat23/springjdbc/service/StudentService.java
@@ -1,11 +1,24 @@
 package com.saswat23.springjdbc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saswat23.springjdbc.model.Student;
+import com.saswat23.springjdbc.repository.StudentRepo;
 
 @Service
 public class StudentService {
+
+	private StudentRepo repo;
+	
+	public StudentRepo getRepo() {
+		return repo;
+	}
+	
+	@Autowired
+	public void setRepo(StudentRepo repo) {
+		this.repo = repo;
+	}
 
 	public void addStudent(Student student) {
 		System.out.println("Added student: "+student);
